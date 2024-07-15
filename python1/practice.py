5240cc66570314bb7b89f395b0dc844eaaccc248
hj99life <hj99.life.service@gmail.com>
2024-07-15 13:50:31 +0900

    문자열 함수

--- python1/practice.py
+++ python1/practice.py
@@ -102,3 +102,40 @@ print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
 print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성 
 print(randint(1, 45)) # 1~ 45 이하의 임의의 값 생성 
 
+date = randint(4, 28)
+print("오프라인 스터디 모임 날짜는 매월 " + str(date) + " 일로 선정되었습니다. ")
+
+sentence = '나는 소년입니다'
+print(sentence)
+sentence2 = "파이썬은 쉬워요"
+print(sentence2)
+sentence3 = """나는 소년이고, 
+파이썬은 쉬워요"""
+print(sentence3)
+
+jumin = "990120-1234567"
+
+print("성별 : " + jumin[7])
+print("연 : " + jumin[0:2]) # 0 ~ 2 직전까지 (0, 1) 
+print("월 : " + jumin[2:4]) # 01
+print("일 : " + jumin[4:6]) # 01
+print("생년월일 : " + jumin[:6]) #990120 처음부터 6 직전까지 
+print("뒤 7자리 : " + jumin[7:]) #1234567 7부터 끝까지 
+print("뒤 7자리 (뒤에부터) : " + jumin[-7:]) # 맨 뒤에서 7번째부터 끝까지 
+
+python = "Python is Amazing"
+print(python.lower()) 
+print(python.upper())
+print(python[0].isupper())
+print(len(python))
+print(python.replace("Python", "Java"))
+
+index = python.index("n")
+print(index)
+index = python.index("n", index + 1)
+print(index)
+
+print(python.find("java"))
+print(python.index("Java"))
+print("hi")
+print(python.count("n"))
