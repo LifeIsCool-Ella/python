b060a60ee49461861e3caa18195d42d1cb644648
hj99life <hj99.life.service@gmail.com>
2024-07-15 16:39:15 +0900

    문자 포맷

--- python1/practice.py
+++ python1/practice.py
@@ -136,6 +136,27 @@ index = python.index("n", index + 1)
 print(index)
 
 print(python.find("java"))
-print(python.index("Java"))
+#print(python.index("Java"))
 print("hi")
 print(python.count("n"))
+
+print("a" + "B")
+print("a", "bb")
+
+print("난 %d 살입니다." % 20)
+print("난 %s 을 좋아해요." % "파이썬")
+print("Apple %c 로 시작해요" % "A")
+
+# %s
+print("나는 %s 살입니다." % 20)
+print("나는 %s 색과 %s 색을 좋아해요. " % ("파란", "빨간"))
+
+print("나는 {} 살입니다.".format(20))
+print("나는 {1} 색과 {0} 색을 좋아해요.".format("파란", "빨간"))
+
+print("나는 {age}살이며, {color}색을 좋아해요.".format(age = 20, color = "빨간"))
+
+# 파이썬 3.6 이상 
+age = 20 
+color = "빨간"
+print(f"나는 {age}살이며, {color}색을 좋아해요.")
