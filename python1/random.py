2d740d535a638b4a8a269e56df766764e7da8519
hj99life <hj99.life.service@gmail.com>
2024-07-18 16:36:27 +0900

    파이썬 기초 예제

---
+++ python1/random.py
@@ -0,0 +1,14 @@
+from random import *
+lst = [1,2,3,4,5]
+print(lst)
+#shuffle(lst)
+print(lst)
+#print(sample(lst,1))
+
+users = range(1,21) #1부터 20까지 숫자를 생성
+print(type(users))
+users = list(users)
+print(type(users))
+
+shuffle(users)
+print(users)
