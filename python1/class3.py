c97af162c9250e1b5c675acd8be27dc85789cde8
hj99life <hj99life@gmail.com>
2024-07-22 16:08:35 +0900

    다중상속 메소드 오버라이딩

--- python1/class3.py
+++ python1/class3.py
@@ -1,12 +1,17 @@
-#상속
 class Unit:
-    def __init__(self, name, hp): # 생성자 
+    def __init__(self, name, hp, speed): # 생성자 
         self.name = name
         self.hp = hp
+        self.speed = speed
         
+    def move(self, location):
+        print("[지상 유닛 이동]")
+        print("{0} : {1} 방향으로 이동합니다. [속도 {2}]"\
+            .format(self.name, location, self.speed))
+
 class AttackUnit(Unit):
-    def __init__(self, name, hp, damage): # 생성자 
-        Unit.__init__(self, name, hp)
+    def __init__(self, name, hp, speed, damage): # 생성자 
+        Unit.__init__(self, name, hp, speed)
         self.damage = damage
         
     def attack(self, location):
@@ -18,14 +23,8 @@ class AttackUnit(Unit):
         self.hp -= damage
         print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
         if self.hp <=0:
-            print("{0} : 파괴되었습니다.".format(self.name))        
-            
-firebat1 = AttackUnit("파이어뱃", 50, 16)
-firebat1.attack("5시")
-
-firebat1.damaged(25)
-firebat1.damaged(25)            
-
+            print("{0} : 파괴되었습니다.".format(self.name))             
+        
 class Flyable:
     def __init__(self, flying_speed):
         self.flying_speed = flying_speed
@@ -36,9 +35,18 @@ class Flyable:
         
 class FlyableAttactUnit(AttackUnit, Flyable):
     def __init__(self, name, hp, damage, flying_speed):
-        AttackUnit.__init__(self, name, hp, damage)
-        Flyable.__init__(self, flying_speed)
+        AttackUnit.__init__(self, name, hp, 0,  damage)
+        Flyable.__init__(self, flying_speed)     
+    
+    def move(self, location):
+        print("[공중 유닛 이동]")   
+        self.fly(self.name, location)
         
-valkyrie = FlyableAttactUnit("발키리", 200, 6,5)        
-valkyrie.fly(valkyrie.name,"3시")
+#
+vulture = AttackUnit("벌쳐", 80 , 10 , 20 )
 
+battlecruiser = FlyableAttactUnit("배틀크루저", 500, 25, 3)
+
+vulture.move("11시")
+battlecruiser.fly(battlecruiser.name, "9시")
+battlecruiser.move("9시")
