3495a80d481602db8b65b4f0d2cdc1a9db68379f
hj99life <hj99life@gmail.com>
2024-07-22 10:58:17 +0900

    파일쓰기 with

--- python1/pickle_exam.py
+++ python1/pickle_exam.py
@@ -11,3 +11,11 @@ profile = pickle.load(profile_file)
 print(profile)
 profile_file.close()
 
+with open("profile.pickle", "rb") as profile_file:
+    print(pickle.load(profile_file))
+    
+    
+with open("study.txt", "w", encoding="utf8 ") as study_file:
+    study_file.write("파이썬 예제 중")
+with open("study.txt", "r", encoding="utf8") as study_file:
+    print(study_file.read())
