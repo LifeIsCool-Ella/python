70c8d88c3cb0843fd4bcebd99778bddd04b57936
hj99life <hj99life@gmail.com>
2024-07-23 14:44:06 +0900

    나도코딩 무료강의 끝

---
+++ python1/pip install.py
@@ -0,0 +1,11 @@
+#pypi
+
+# pip install beautifulsoup4
+# pip list 
+# pip show beautifulsoup4
+# pip install --upgrade beautifulsoup4
+# pip uninstall beautifulsoup4
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup("<p>Some<b>bad<i>HTML")
+print(soup.prettify())
