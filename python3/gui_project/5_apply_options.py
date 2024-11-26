6f942ac48b0f49cdc8fda88bf3cd898bafc7ab2d
hj99life <hj99life@gmail.com>
2024-11-26 14:50:08 +0900

    파이썬 활용편2

--- python3/gui_project/5_apply_options.py
+++ python3/gui_project/5_apply_options.py
@@ -36,19 +36,42 @@ def browse_dest_path():
     txt_dest_path.insert(0, folder_selected)
     
 def merge_image():
+    
+    img_width = cmb_width.get()
+    if img_width =="원본유지":
+        img_width = -1 
+    else:
+        img_width = int(img_width)
+    
+    img_space = cmb_space.get()
+    if img_space == "좁게":
+        img_space = 30
+    elif img_space == "보통":
+        img_space = 60
+    elif img_space == "넓게":
+        img_space = 90
+    else:
+        img_space = 0 
+        
+    img_format = cmb_format.get().lower() 
+        
     #print(list_file.get(0, END))    
     images = [Image.open(x) for x in list_file.get(0, END)]
-    # size -> size[0] : width, size[1]:height
-    # widths = [x.size[0] for x in images]
-    # heights = [x.size[1] for x in images]
+        
+    image_sizes = [] 
+    if img_width > -1: 
+        image_sizes = [(int(img_width), int(img_width * x.size[1] / x.size[0]))]
+    else : 
+        image_sizes = [(x.size[0], x.size[1]) for x in images]
+       
     
-    #max_width, total_height = max(widths), sum(heights) 
-    #print("max width : ", max_width)
-    #print("total height : ", total_height)
-    
-    widths, heights = zip(*(x.size for x in images))
+    #widths, heights = zip(*(x.size for x in images))
+    widths, heights = zip(*(image_sizes))
     
     max_width, total_height = max(widths), sum(heights)
+    
+    if img_space > 0:
+        total_height += (img_space * (len(images) - 1))
     
     result_img = Image.new("RGB", (max_width, total_height), (255, 255, 255))
     y_offset = 0 
@@ -57,15 +80,20 @@ def merge_image():
     #    y_offset += img.size[1]
         
     for idx, img in enumerate(images):
+        if img_width > -1: 
+            img = img.resize(image_sizes[idx])
+            
+            
         result_img.paste(img, (0, y_offset))
-        y_offset += img.size[1]
+        y_offset += ( img.size[1] + img_space )
         
-        progress = (idx + 1) / len(images)
+        progress = (idx + 1) / len(images) * 100
         p_var.set(progress)
         progress_bar.update()
         
-        
-    dest_path = os.path.join(txt_dest_path.get(), "merge_photo.jpg")
+     
+    file_name = "merge_photo."+ img_format    
+    dest_path = os.path.join(txt_dest_path.get(), file_name)
     result_img.save(dest_path)
     msgbox.showinfo("알림", "작업이 완료되었습니다.")
     
