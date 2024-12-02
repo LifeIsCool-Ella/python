f9d6ebe151f101db607bd33340b59d8836ab8cbf
hj99life <hj99life@gmail.com>
2024-12-02 23:12:46 +0900

    오타 수정

--- python3/gui_basic/12_frame.py
+++ python3/gui_basic/12_frame.py
@@ -6,7 +6,7 @@ root.geometry("640x480")
 
 Label(root, text="메뉴를 선택해 주세요").pack(side="top")
 
-BUtton(root, text="주문하기").pack(side="bottom")
+Button(root, text="주문하기").pack(side="bottom")
 
 frame_burger = Frame(root, relief="solid", bd=1)
 frame_burger.pack(side="left", fill="both", expand=True)
